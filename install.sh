d19411a93a5885b033543bda6fb5e44249811da6
mckeankylej <mckean.kylej@gmail.com>
2016-06-09 10:56:51 -0400

    p3

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 post_install() {
 	# info "Kyle McKean's .spacemac.d downloaded"
 	mkdir -p "$HOME/.spacemacs.d"
-	cp -r "$PEARL_PKGDIR/spacemacs.d" "$HOME/.spacemacs.d"
+	cp -r "$PEARL_HOME/packages/spacemacs.d" "$HOME/.spacemacs.d"
 }
 
