9e3b763210bfca3ca2ba8b6d57b88bc8db22980a
mckeankylej <mckean.kylej@gmail.com>
2016-06-09 11:10:47 -0400

    p5

--- pearl-metadata/install.sh
+++ pearl-metadata/install.sh
@@ -1,6 +1,6 @@
+set -x
 post_install() {
-	# info "Kyle McKean's .spacemac.d downloaded"
+	info "Kyle McKean's .spacemac.d downloaded"
 	mkdir -p "$HOME/.spacemacs.d"
-	cp -r "$PEARL_HOME/packages/spacemacs.d" "$HOME/.spacemacs.d"
+        link "$HOME/.spacemacs.d/init.el" "$PEARL_HOME/packages/spacemacs.d/init.el"	
 }
-
